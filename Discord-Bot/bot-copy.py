13b2b4e36188e92d52e51d73e37855c41775dad3
marwa qureshi <qureshi.19@wright.edu>
2022-02-07 01:45:00 -0500

    testing my code to see why bot was not appearing

--- Discord-Bot/bot-copy.py
+++ Discord-Bot/bot-copy.py
@@ -13,40 +13,74 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-        print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
-        for guild in client.guilds:
-            if guild.name == GUILD:
-                break
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-        print(
-            f'{client.user} is connected to the following guild:\n'
-            f'{guild.name}(id: {guild.id})'
-         )                                                           
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}'
+          )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
 @client.event
 async def on_message(message):
-        if message.author == client.user:
-            return
+    if message.author == client.user:
+        return
 
-        brooklyn_99_quotes = [
-            'I\'m the human form of the 💯 emoji.',
-            'Bingpot!',
-            (
-                'Cool. Cool cool cool cool cool cool cool, '
-                'no doubt no doubt no doubt no doubt.'
-            ),
-        ]
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-        hitchhiker_quotes = [
-            'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-            'It is a mistake to think you can solve any major problems just with potatoes.',
-            'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-            'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-        ]
+    hitchhiker_quotes = [
+        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
+        'It is a mistake to think you can solve any major problems just with potatoes.',
+        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
+        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    ]
+    
+    hello_quote = [
+            'Heyyy, I am the famous bot that you have been looking for!',
+            'whatsuppppp',
+            'Hi, How are you?',
+            'Peek A Boo. Hi!'
+    ]
 
-        if message.content == 'towel!':
-            #response = random.choice(brooklyn_99_quotes)
-            response = random.choice(hitchhiker_quotes)
-            await message.channel.send(response)
+    bye_quote = [
+            'Good Bye',
+            'See You Later',
+            'Aww I will miss you'
+    ]
+
+
+    if message.content == 'towel!':
+        #response = random.choice(brooklyn_99_quotes)
+        response = random.choice(hitchhiker_quotes)
+        await message.channel.send(response)
+
+    elif message.content == 'Hi':
+        response = random.choice(hello_quote)
+        await message.channel.send(response)
+
+    elif message.content == 'Bye':
+        response = random.choice(bye_quote)
+        await message.channel.send(response)
 
 client.run(TOKEN)
